53e0be26c9cd32264918f85cb8dfd40061cd1580
DarkMG1 <driverstation8728@gmail.com>
2023-01-26 21:00:38 -0500

    Update Robot.java

--- src/main/java/org/troyargonauts/Robot.java
+++ src/main/java/org/troyargonauts/Robot.java
@@ -29,8 +29,9 @@ public class Robot extends TimedRobot {
     public void robotInit() {
         // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
         // autonomous chooser on the dashboard.
-        robotContainer = new RobotContainer();
         gearbox = new Gearbox();
+        robotContainer = new RobotContainer();
+
     }
 
     @Override
